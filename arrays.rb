87165e84d75b4a141d062f35f2f00963b2c3ca6b
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-05-31 15:43:34 -0600

    Call on the 4 methods pop, push, shift, unshift

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,14 @@ pets = ["Gimli", "Boo", "Marcy", "Emi", "Harleigh"]
 ages = [3, 1, 2, 6, 4]
 menu_prices = [5.5, 4.5, 2.5, 4.0]
 has_graduated = [true, false, false, true, true]
+# This will pull the last element of the "pets" array
+pets.pop
+
+# This will push the provided elements to the end of the ages array
+ages.push(7, 8, 9)
+
+# This will remove the first element (5.5) from the menu prices array and will return it!
+menu_prices.shift
+
+# This will add the elements to the beginning of the array
+has_graduated.unshift(true, false)
